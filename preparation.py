d0afdc5845e5abd95a62ae9c6a7f86c60b7258d8
xiki-tempula <zczlzwu@ucl.ac.uk>
2015-07-13 09:17:24 +0100

    change impose resolution

--- preparation.py
+++ preparation.py
@@ -24,7 +24,7 @@ def filter_first_last(open_period, shut_period):
         return False
     return open_period, shut_period
 
-def impose_resolution(start, end, 
+def impose_resolution(start, end,
                       open_period, shut_period,
                       open_amp, shut_amp,
                       open_flag, shut_flag,
@@ -36,7 +36,7 @@ def impose_resolution(start, end,
     assert len(open_period) == len(shut_period)
     assert len(open_amp) == len(shut_amp)
     assert len(open_flag) == len(shut_flag)
-    
+
     # Prepare the data
     cluster_length = len(open_period)*2
     period = np.empty(cluster_length)
@@ -51,84 +51,100 @@ def impose_resolution(start, end,
     state = np.empty(cluster_length, dtype=bool)
     state[::2] = True
     state[1::2] = False
-    
+
     # Find out the periods which is unresolvable
     unresolvable = np.where(period < resolution)[0]
 
     # Cluster the unresolvable into consecutive compoents
     unresolvable = np.split(unresolvable, np.where(np.diff(unresolvable) != 1)[0]+1)
-    
+
     for index in unresolvable:
-        # If this index does not include the first period and the last period
+        # If this index does not include the first period
+        # Merge all the periods in the index with the previous one
         if index[0] != 0:
-            # If the period before the first element of unresolvable list is
-            # the same as the period after the last unresolvable list.
-            # Merge all the periods in between
-            if (state[index[0]-1] == state[index[-1]+1]) and (index[-1] != cluster_length-1):
-                new_amp = (amp[index[0]-1] + amp[index[-1]+1])/2
-                index = np.hstack((index, index[-1]+1, index[0]-1))
-            # if not leave the next resolvable period untouched
-            else:
-                new_amp = amp[index[0]-1]
-                index = np.hstack((index, index[0]-1))
-                
+            new_amp = amp[index[0]-1]
+            index = np.hstack((index, index[0]-1))
             new_state = state[index[0]-1]
+        # Since there is no previous one
+        # Merge all the periods in the index
         else:
             new_state = False
-            if state[index[-1]+1]:
-                new_amp = np.nan
-            else:
-                new_amp = amp[index[-1]+1]
-                index = np.hstack((index, index[-1]+1))
-                
+            new_amp = np.nan
+
+        # Merge all the periods
         new_period = sum(period[index])
+        # Detect bad flag
         new_flag = BROKEN if any([flag[i] == BROKEN for i in index]) else NORMAL
-        
+
         # Delete all the value in this range
         amp[index] = np.nan
         period[index] = np.nan
         flag[index] = np.nan
         state[index] = np.nan
-        
-        # Fill in new value
+
+        # Fill in the merged value
         amp[index[0]] = new_amp
         period[index[0]] = new_period
         flag[index[0]] = new_flag
         state[index[0]] = new_state
-    
-    # Filter out all the NaN   
-    valid = np.isfinite(period)    
+
+    # Filter out all the NaN
+    valid = np.isfinite(period)
+
+    # Find the first open and last shut
+    first = np.where(state[valid] = True)[0][0]
+    last = np.where(state[valid] = False)[0][-1] + 1
+
+    # Update the valid range
+    valid = valid[first:last]
+
+    # Update all the value
+    start += np.nansum(period[:first])
+    end -= np.nansum(period[last:])
     amp = amp[valid]
     period = period[valid]
     flag = flag[valid]
     state = state[valid]
-    
-    # Check head and tail
-    if not state[0]:
-        start += period[0]
-        head = 1
-    else:
-        head = 0
-        
-    if state[-1]:
-        end -= period[-1]
-        tail = -1
-    else:
-        tail = None
-    
-    # Chop off the head or tail if the head is shut or the tail is open
-    if (head == 1) or (tail == -1):
-        amp = amp[head:tail]
-        period = period[head:tail]
-        flag = flag[head:tail]
-        state = state[head:tail]
-    
+
+    # Check for the consecutive open and shut
+    open_index  = np.where(state == True)[0]
+    shut_index  = np.where(state == False)[0]
+    consecutive_open = np.split(open_index, np.where(np.diff(open_index) == 1)[0]+1)
+    consecutive_shut = np.split(shut_index, np.where(np.diff(shut_index) == 1)[0]+1)
+    consecutive = consecutive_open + consecutive_shut
+
+    if len(consecutive) > 0:
+        # Merge all the consecutive open and shut
+        for index in consecutive:
+            new_amp = np.mean(amp[index])
+            new_period = np.sum(period[index])
+            new_state = state[index][0]
+            new_flag = BROKEN if any([flag[i] == BROKEN for i in index]) else NORMAL
+
+            # Delete all the value in this range
+            amp[index] = np.nan
+            period[index] = np.nan
+            flag[index] = np.nan
+            state[index] = np.nan
+
+            # Fill in the merged value
+            amp[index[0]] = new_amp
+            period[index[0]] = new_period
+            flag[index[0]] = new_flag
+            state[index[0]] = new_state
+
+        # Get rid of the NaN
+        valid = np.isfinite(period)
+        amp = amp[valid]
+        period = period[valid]
+        flag = flag[valid]
+        state = state[valid]
+
     open_period = period[::2]
     shut_period = period[1::2]
     open_amp = period[::2]
     shut_amp = period[1::2]
     open_flag = period[::2]
     shut_flag = period[1::2]
-    
+
     return start, end, open_period, shut_period, open_amp, shut_amp, open_flag, shut_flag
-    
